9f26b1efc8dc32b25ec6311ec86753ee7d9a2f48
Rafael Quintanilha <rafaelquintanilha@poli.ufrj.br>
2019-07-12 13:16:03 -0400

    Added analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-64315802-4');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
